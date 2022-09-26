68824bdcf77259b0553f2a54011073ce898bbbdf
Rafi Muhammad <rafi.muhammad@mekari.com>
2022-09-26 23:02:52 +0700

    Change deploy config

--- main.go
+++ main.go
@@ -12,16 +12,12 @@ func main() {
 		port = "8080"
 	}
 
-	server := http.Server{
-		Addr: "0.0.0.0:" + port,
-	}
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!!"))
 	})
 
-	log.Printf("Server running on %s", server.Addr)
-	err := server.ListenAndServe()
+	log.Printf("Server running on port %s", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Println("Error starting server: ", err)
 		panic(err)
